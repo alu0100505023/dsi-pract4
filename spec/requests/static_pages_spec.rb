155bfe065013e9648ba77340bcd65caed271d49e
Javier CRG <alu0100505023@ull.edu.es>
2014-03-05 11:35:55 +0000

    Refactorizando errores en los titulos de las paginas

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -8,6 +8,12 @@ describe "Static pages" do
       visit '/static_pages/home'
       expect(page).to have_content('prct4-rails')
     end
+
+	it "should have the title 'Home'" do
+      visit '/static_pages/home'
+      expect(page).to have_title("Ruby on Rails Tutorial prct4-rails | Home")
+    end
+
   end
 
 	describe "Help page" do
@@ -16,6 +22,12 @@ describe "Static pages" do
       visit '/static_pages/help'
       expect(page).to have_content('Ayuda')
     end
+
+	 it "should have the title 'Help'" do
+      visit '/static_pages/help'
+      expect(page).to have_title("Ruby on Rails Tutorial prct4-rails | Help")
+    end
+
   end	
 
 	describe "About page" do
@@ -23,6 +35,11 @@ describe "Static pages" do
     it "should have the content 'About Us'" do
       visit '/static_pages/about'
       expect(page).to have_content('About Us')
+    end
+
+	it "should have the title 'About Us'" do
+      visit '/static_pages/about'
+      expect(page).to have_title("Ruby on Rails Tutorial prct4-rails | About Us")
     end
   end
 end
